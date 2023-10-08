71d5231223ad13e12e8cda62a80b032d206bc3ad
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2023-10-08 17:49:22 -0500

    figured out while loop

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -46,16 +46,26 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
-
+for(star of stars){
+  console.log(star)
+}
 
 // 3. While loops  
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
-
+let i=0;
+while(i<stars.length){
+  console.log(stars[i]);
+  i++;
+}
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
-
+let y=0;
+while(i<=5){
+  console.log(y);
+  y++;
+}
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
